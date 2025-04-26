ca0b5ba1a999adac30178fdddf06e1986d67f38b
MohamadWebdev <mohamad.webdev99@gmail.com>
2025-04-26 11:14:30 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='w-full'>
+      <div className="w-full max-w-[1002.34px] mx-auto min-[1000px]:px-20 px-5">
+        {/* Navbar */}
+        <Navbar/>
+        {/* Hero */}
+        {/* Clients */}
+        {/* Community */}
+        {/* Unlock */}
+        {/* Achievements */}
+        {/* Calender */}
+        {/* Customers */}
+        {/* CommunityUpdates */}
+        {/* Footer */}
+      </div>
+    </div>
   )
 }
 
