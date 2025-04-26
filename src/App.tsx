0dadd3f547b0fc58353ee7dd4e988f1c51554840
MohamadWebdev <mohamad.webdev99@gmail.com>
2025-04-26 12:05:19 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar"
+import { UseContext } from "./context/contextProvider"
 
 const App = () => {
+  const {toggle}:any = UseContext()
   return (
-    <div className='w-full'>
+    <div className={`w-full ${toggle} h-full overflow-y-auto z-0`}>
       <div className="w-full max-w-[1002.34px] mx-auto min-[1000px]:px-20 px-5">
         {/* Navbar */}
         <Navbar/>
